bfb3cc8555c9448fe09b2f60c8b1a1e45487fd6f
Hampus B <hampus.blinke@elev.ga.ntig.se>
2022-10-26 11:11:11 +0200

    Calc completed

--- Calc/script.js
+++ Calc/script.js
@@ -1,13 +1,69 @@
 const elemBtnCalc = document.getElementById("btn-calc");
-
-
 const elemCalcScreen = document.getElementById("calc-screen");
+const elemMain = document.getElementById("calc");
 
-function btnCalc() {
+
+function btnCalc() {                    // This function takes care of the calculation  
+    /* your code here */
+    sum = result;
+
+    elemCalcScreen.innerHTML = sum;     // show the result on screen
+}
+
+function addDigit(thisSign) {           // Add a digit
+    elemCalcScreen.innerHTML += thisSign;
+}
+
+function rotate() {
+    elemMain.classList.toggle("rotate");
+
+}
+
+
+function btnConvertToEuro() {           //Converts it to euro
+    let x = elemCalcScreen.innerHTML;
+
     let sum = 0;
-    let x = 10;
-    let y = 5;
+
+    sum = x/10.9;
+    sum = sum.toFixed(2);   //toFixed makes it so only 2 decimals shows on the screen
+
+    if (isNaN(sum)) {
+        elemCalcScreen.innerHTML = "ERROR" //Makes it so when NaN (Not a Number) woudl apear on the screen a error message instead appears
+    }
+    else {
+        elemCalcScreen.innerHTML = sum;
+    }
+
+}
+
+function btnConvertToDollar() {         //Converts to dollar
+   let x = elemCalcScreen.innerHTML;
+
+    let sum = 0;
+
+    sum = x/11.1;
+    sum = sum.toFixed(2);      //toFixed makes it so only 2 decimals shows on the screen
+
+    if (isNaN(sum)) {
+        elemCalcScreen.innerHTML = "ERROR" //Makes it so when NaN (Not a Number) woudl apear on the screen a error message instead appears
+    }
+    else {
+        elemCalcScreen.innerHTML = sum;
+    }
+}
+
+function btnBackSpace() {               //Removes the number furthest to the right  
+    let calcScreen = elemCalcScreen.innerHTML;
+    calcScreen = calcScreen.slice(0, -1);
+    elemCalcScreen.innerHTML = calcScreen;
     
-    sum = x + y;
-    elemCalcScreen.innerHTML = sum;
+}
+
+function btnClearAll () {                 //Removes everything on the screen
+ let x = calcScreen = elemCalcScreen.innerHTML;
+
+ calcScreen = ''
+
+ elemCalcScreen.innerHTML = calcScreen;
 }
